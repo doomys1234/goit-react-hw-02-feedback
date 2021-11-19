f8a3e85edda2beb4f6240357c2e057d9b09848d2
doomys1234 <doomys1234@gmail.com>
2021-11-19 20:56:17 +0200

    added files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Feedback from './Feedback/Feedback';
+import Statistics from './Statistics/Statistics';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Feedback />
+      <Statistics />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Statistics/Statistics.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Statistics() {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good:</p>
+      <p>Neutral:</p>
+      <p>Bad:</p>
+    </div>
+  );
+}
+
+export default Statistics;
